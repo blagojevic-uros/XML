fb2bfe28400469089f455f9386fb44a8b9fce672
Nikola Petrovic <s.nino.petrovic@gmail.com>
2022-07-12 09:25:05 +0200

    xslfo path

--- Spring-JAX-RS/src/main/java/util/XSLFOPaths.java
+++ Spring-JAX-RS/src/main/java/util/XSLFOPaths.java
@@ -6,7 +6,7 @@ public class XSLFOPaths {
     public static final String ZAHTEV_XSL_FO = "data/xsl-fo/zahtev_fo.xsl";
     public static final String SERTIFIKAT_XSL_FO = "data/xsl-fo/sertifikat_fo.xsl";
     public static final String INTERESOVANJE_XSL_FO = "Spring-JAX-RS/data/xsl-fo/interesovanje_fo.xsl";
-    public static final String POTVRDA_XSL_FO = "data/xsl-fo/potvrda_fo.xsl";
+    public static final String POTVRDA_XSL_FO = "Spring-JAX-RS/data/xsl-fo/potvrda_fo.xsl";
     public static final String SAGLASNOST_XSL_FO = "data/xsl-fo/saglasnost_fo.xsl";
 
 }
--- Spring-JAX-RS/src/main/java/util/XSLPaths.java
+++ Spring-JAX-RS/src/main/java/util/XSLPaths.java
@@ -5,6 +5,6 @@ public class XSLPaths {
     public static final String SERTIFIKAT_XSL = "Spring-JAX-RS/data/xsl/sertifikat.xsl";
     public static final String INTERESOVANJE_XSL = "Spring-JAX-RS/data/xsl/interesovanje.xsl";
     public static final String ZAHTEV_XSL = "data/xsl/zahtev.xsl";
-    public static final String POTVRDA_XSL = "data/xsl/potvrda.xsl";
+    public static final String POTVRDA_XSL = "Spring-JAX-RS/data/xsl/potvrda.xsl";
     public static final String SAGLASNOST_XSL = "data/xsl/saglasnost.xsl";
 }
